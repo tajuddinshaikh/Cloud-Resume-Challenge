2944d9033715b4d0d083328ea72bfc6a841b7f31
Tajuddin Shaikh <121308030+tajuddinshaikh@users.noreply.github.com>
2023-04-10 10:14:10 -0400

    Update index.js

--- CloudResumeChallenge/index.js
+++ CloudResumeChallenge/index.js
@@ -1,7 +1,9 @@
-const counter = document.querySelector(".viewscount");
+const counter = document.querySelector(".viewcount");
 async function updateCounter(){
-    let response = await fetch("");
+    let response = await fetch("https://6lor4hi3ruqxgj4crhyey2y4ya0brdqi.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `This page has been viewed ${data} times`;
 
 }
+
+updateCounter();
