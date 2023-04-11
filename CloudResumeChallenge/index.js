df287e9a923022afdc6d55f34f4addf1601a18d0
Tajuddin Shaikh <121308030+tajuddinshaikh@users.noreply.github.com>
2023-04-10 20:56:06 -0400

    Delete index.js

--- CloudResumeChallenge/index.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".viewcount");
-async function updateCounter(){
-    let response = await fetch("https://6lor4hi3ruqxgj4crhyey2y4ya0brdqi.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `This page has been viewed ${data} times`;
-
-}
-
-updateCounter();
